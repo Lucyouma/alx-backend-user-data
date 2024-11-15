7b08d4bfabd7cfb9e753954d6021f0799b2c7b6f
Lucyouma <akinyilucy09@gmail.com>
2024-11-15 09:13:10 -0500

    added template for authentication class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,34 @@
+from flask import request
+"""
+module template for
+authentication class
+"""
+
+
+class Auth:
+    """
+    class to manage API authentication
+    """
+    def __init__(self):
+        """
+        class initialization
+        """
+        self.data = []
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns whether authentication is required
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        returns the authorization header in a request
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns the current user
+        """
+        return None
