f0ede5be780baef685cef962949c8159e8eb8cc8
Lucyouma <akinyilucy09@gmail.com>
2024-11-15 16:25:10 -0500

    added method to retrieve user object from credentials and one to retrieve a user instance for a request  with basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,8 @@ module for basic authentication
 
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -67,3 +69,35 @@ class BasicAuth(Auth):
             return None, None
         creds = decoded_base64_authorization_header.split(':', 1)
         return creds[0], creds[1]
+
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """
+        returns instance for user given email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            exist_user = User.search({'email': user_email})
+
+        except Exception as e:
+            return None
+
+        for user in exist_user:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        retrieves a user instance for a request made withbais auth
+        """
+        encoded = self.authorization_header(request)
+        user = self.extract_base64_authorization_header(encoded)
+        decoded = self.decode_base64_authorization_header(user)
+        email, pwd = self.extract_user_credentials(decoded)
+        user = self.user_object_from_credentials(email, pwd)
+        return user
