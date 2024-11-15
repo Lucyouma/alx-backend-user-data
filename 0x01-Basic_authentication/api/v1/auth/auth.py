b851dbf1bca7ca1695e23bbf7d7d621b578d052b
Lucyouma <akinyilucy09@gmail.com>
2024-11-15 13:26:00 -0500

    added the function to check for authorized paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,8 +20,20 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         returns whether authentication is required
+        - returns True if path is None or
+        if excluded_path is empty or None
+        - Handles paths to be slash tolerant
         """
-        return False
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        path = path.rstrip('/')
+        excluded_paths = [p.rstrip('/') for p in excluded_paths]
+
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """
