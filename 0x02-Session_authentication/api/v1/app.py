0535e7b85c40d56d1e9c9ec342382931180f68ea
Lucyouma <akinyilucy09@gmail.com>
2024-11-17 14:01:52 -0500

    updated before_request to add login path

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,7 +58,7 @@ def before_request() -> str:
     request
     """
     path_list = ['/api/v1/status/', '/api/v1/unauthorized/',
-                 '/api/v1/forbidden/']
+                 '/api/v1/forbidden/', '/api/v1/auth_session/login/']
 
     if auth is None:
         return
@@ -66,7 +66,8 @@ def before_request() -> str:
     if not auth.require_auth(request.path, path_list):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
