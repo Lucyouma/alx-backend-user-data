efdd4817f5d58215f5886a1ff1f41bbee2dbc801
Lucyouma <akinyilucy09@gmail.com>
2024-11-17 12:15:14 -0500

    added Session Authentication class and updated authentication check to accomodate Session Authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+module for session authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session authentication class
+    inherits from Auth
+    """
+    pass
