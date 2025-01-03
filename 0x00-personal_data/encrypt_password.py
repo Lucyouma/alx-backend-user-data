44beb6f3c7c1cfef8794110fba14416a03a6985c
Lucy Ouma <akinyilucy09@gmail.com>
2025-01-03 05:59:37 +0300

    Personal data upates

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,12 +5,12 @@ Encrypting passwords
  import bcrypt
 
 
- def hash_password(password: str) -> bytes:
-     """ Returns a salted, hashed password, which is a byte string """
-     encoded = password.encode()
-     hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+def hash_password(password: str) -> bytes:
+    """ Returns a salted, hashed password, which is a byte string """
+    encoded = password.encode()
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
 
-     return hashed
+    return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
